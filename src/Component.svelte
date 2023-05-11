8e89fd0c12583e858a8b9000afdd3322b3e710dd
Mel O'Hagan <mel@budibase.com>
2023-05-11 16:33:03 +0100

    Closes #17 Don't bind value

--- src/Component.svelte
+++ src/Component.svelte
@@ -146,7 +146,7 @@
     </label>
   </div>
     <DateInput 
-      bind:value={fieldState.value} 
+      value={fieldState.value} 
       on:select={e => {
         const changed = fieldApi?.setValue(e.detail)
         if (onChange && changed) {
